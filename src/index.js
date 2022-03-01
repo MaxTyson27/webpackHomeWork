b3a50a500ffc5767bddc04e3522456c40fba983e
MaxTyson27 <72412722+MaxTyson27@users.noreply.github.com>
2022-03-01 22:54:58 +0300

    added homework to lesson 23

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import menu from './modules/menu'
 import modal from './modules/modal';
 import validate from './modules/validate';
 import tabs from './modules/tabs';
+import slider from './modules/slider';
 
 one('25 february 2022')
 menu()
 modal()
 validate()
 tabs()
+slider('.portfolio-content', '.portfolio-item')
